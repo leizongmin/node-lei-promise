573ed46a39df3e8c186c257505a5266b65cc669e
Zongmin Lei <leizongmin@gmail.com>
2016-01-01 14:00:27 +0800

    test callbackify()

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ describe('promisify', function () {
       }, ms);
     }
 
-    var sleep2 = me.promisify(sleep);
+    let sleep2 = me.promisify(sleep);
 
     sleep2(100).then(([ms]) => {
       console.log(ms);
@@ -43,7 +43,7 @@ describe('promisify', function () {
       }, ms);
     }
 
-    var sleep2 = me.promisify(sleep);
+    let sleep2 = me.promisify(sleep);
 
     sleep2(100, 'ok').then(([msg, ms]) => {
       console.log(msg, ms);
@@ -62,19 +62,141 @@ describe('promisify', function () {
       console.log(`sleep(${ms})`);
       setTimeout(function () {
         console.log(`sleep(${ms}) callback`);
-        var err = new Error('just test');
+        let err = new Error('just test');
         err.code = 'just_test';
         callback(err);
       }, ms);
     }
 
-    var sleep2 = me.promisify(sleep);
+    let sleep2 = me.promisify(sleep);
 
     sleep2(100).then(_ => {
       throw new Error('should catch error');
     }).catch(err => {
       assert.equal(err.code, 'just_test');
       done();
+    });
+
+  });
+
+  it('after promisify still callback', function (done) {
+
+    function sleep(ms, callback) {
+      console.log(`sleep(${ms})`);
+      setTimeout(function () {
+        console.log(`sleep(${ms}) callback`);
+        callback(null, ms);
+      }, ms);
+    }
+
+    let sleep2 = me.promisify(sleep);
+
+    sleep2(100, function (err, ms) {
+      console.log(err, ms);
+      assert.equal(err, null);
+      assert.equal(ms, 100);
+      done();
+    });
+
+  });
+
+});
+
+
+describe('callbackify', function () {
+
+  it('passed 1 argument', function (done) {
+
+    function sleep(ms) {
+      console.log(`sleep(${ms})`);
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          console.log(`sleep(${ms}) resolve`);
+          resolve(ms);
+        }, ms);
+      });
+    }
+
+    let sleep2 = me.callbackify(sleep);
+
+    sleep2(100, function (err, ms) {
+      console.log(err, ms);
+      assert.equal(err, null);
+      assert.equal(ms, 100);
+      done();
+    });
+
+  });
+
+  it('passed 2 argument', function (done) {
+
+    function sleep(ms, msg) {
+      console.log(`sleep(${ms})`);
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          console.log(`sleep(${ms}) resolve`);
+          resolve([ms, msg]);
+        }, ms);
+      });
+    }
+
+    let sleep2 = me.callbackify(sleep);
+
+    sleep2(100, 'ok', function (err, [ms, msg]) {
+      console.log(err, ms, msg);
+      assert.equal(err, null);
+      assert.equal(ms, 100);
+      assert.equal(msg, 'ok');
+      done();
+    });
+
+  });
+
+  it('reject', function (done) {
+
+    function sleep(ms) {
+      console.log(`sleep(${ms})`);
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          console.log(`sleep(${ms}) resolve`);
+          var err = new Error('just test');
+          err.code = 'just_test';
+          reject(err);
+        }, ms);
+      });
+    }
+
+    let sleep2 = me.callbackify(sleep);
+
+    sleep2(100, function (err) {
+      console.log(err);
+      assert.ok(err instanceof Error);
+      assert.equal(err.code, 'just_test');
+      done();
+    });
+
+  });
+
+  it('after callbackify still callback', function (done) {
+
+    function sleep(ms) {
+      console.log(`sleep(${ms})`);
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          console.log(`sleep(${ms}) resolve`);
+          resolve(ms);
+        }, ms);
+      });
+    }
+
+    let sleep2 = me.callbackify(sleep);
+
+    sleep2(100).then(ms => {
+      console.log(ms);
+      assert.equal(ms, 100);
+      done();
+    }).catch(err => {
+      throw err;
     });
 
   });
